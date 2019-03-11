7338b10ec35d71b27aebf38ca8ccaa1333b049b4
Yifei <yifei_yin@outlook.com>
2019-03-11 13:32:28 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+
+
+def main():
+  readFile("pathfinding_a.txt")
+
+
+
+## convert input file to list representation of 
+def readFile(filename):
+  file = open(filename, "r")
+  arr = []
+  for line in file:
+    arr.append(list(line[:-1]))
+  return arr
+
+
+def
+
+
+
+
+main()
