03e5c4d704d54f5087227a5c9686da24886c3d48
Yifei <yifei_yin@outlook.com>
2019-03-11 14:06:20 -0400

    read in file finished

--- main.py
+++ main.py
@@ -3,20 +3,41 @@
 
 
 def main():
-  readFile("pathfinding_a.txt")
+  maze,start,goal = readFile("pathfinding_a.txt")
+
+  showMaze(maze)
 
 
-
-## convert input file to list representation of 
+## convert input file to list representation of the maze
+## return maze, start coordinates, goal coordinates
 def readFile(filename):
   file = open(filename, "r")
   arr = []
+  start = [0,0]
+  goal = [0,0]
+
   for line in file:
-    arr.append(list(line[:-1]))
-  return arr
+    if line[-1] == '\n':
+      line = line[:-1]
+    arr.append(list(line))
 
+  maze = []
+  for i in range(len(arr)):
+    line = arr[i]
+    for j in range(len(line)):
+      token = line[j]
+      if token == "S":
+        start = [i,j]
+      elif token == "G":
+        goal = [i,j]
+    maze.append(line)
+  return maze, start, goal
 
-def
+## prints the maze
+def showMaze(maze):
+  for row in maze:
+    print(row)
+
 
 
 
